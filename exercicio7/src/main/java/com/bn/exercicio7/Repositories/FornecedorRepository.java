f36e5a999d6cb605d138501ce71b3a8a68da0ac7
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-18 22:12:25 -0300

    Criação do Repository

---
+++ exercicio7/src/main/java/com/bn/exercicio7/Repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.bn.exercicio7.Repositories;
+
+import com.bn.exercicio7.Models.FornecedorModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Long, FornecedorModel> {
+}
