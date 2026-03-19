b87ecc793edce73f123823c957e64dce4ed58769
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-19 19:45:18 -0300

    Criação do Controller

---
+++ exercicio7/src/main/java/com/bn/exercicio7/Controllers/FornecedorController.java
@@ -0,0 +1,52 @@
+package com.bn.exercicio7.Controllers;
+
+import com.bn.exercicio7.Models.FornecedorModel;
+import com.bn.exercicio7.Repositories.FornecedorRepository;
+import com.bn.exercicio7.Services.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/fornecedores")
+public class FornecedorController {
+
+    @Autowired
+    private FornecedorService fornecedorService;
+
+    @GetMapping
+    public ResponseEntity<List<FornecedorModel>> findAll(){
+        List<FornecedorModel> fornecedor = fornecedorService.findAll();
+        return ResponseEntity.ok().body(fornecedor);
+    }
+
+    @GetMapping("/{id}")
+    public FornecedorModel buscarFornecedorPorId(@PathVariable Long id){
+        return fornecedorService.buscarFornecedorPorId(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<FornecedorModel> criarFornecedor(@RequestBody FornecedorModel fornecedorModel){
+       FornecedorModel fornecedorCriado = fornecedorService.criarFornecedor(fornecedorModel);
+       URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+               .path("/{id}").buildAndExpand(fornecedorModel.getId()).toUri();
+       return ResponseEntity.created(uri).body(fornecedorCriado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarFornecedor(@PathVariable Long id){
+        fornecedorService.deletarFornecedor(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FornecedorModel> atualizarFornecedor
+            (@PathVariable Long id, @RequestBody FornecedorModel fornecedorModel){
+        FornecedorModel fornecedorAtualizado = fornecedorService.atualizarFornecedor(id, fornecedorModel);
+        return ResponseEntity.ok(fornecedorAtualizado);
+    }
+}
